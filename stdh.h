7faf4603256f01597b2c443c9a50db9ae0773ab6
Hyodeon <81826072+Hyodeon@users.noreply.github.com>
2021-04-05 22:06:27 +0900

    Create stdh.h

---
+++ stdh.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define RUNNING_1	100
+#define RUNNING_2	101
+#define JUMPING		102
+
+#define GROUND		12
+#define SPK_RESET	50
+
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <stdlib.h>
+
+typedef int STATUS;
